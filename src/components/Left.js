ff2c58809b44b94efee180405218cd901484924d
chirag dixit <chiragdixit002@gmail.com>
2024-01-07 20:02:48 +0530

    [07/01/24]: Adding templates

---
+++ src/components/Left.js
@@ -0,0 +1,11 @@
+function Left(prop){
+    return (
+        <div className="left" style={{
+            backgroundColor: prop.color
+        }}>
+            <h1>Left</h1>
+        </div>
+    );
+}
+
+export default Left;
---
+++ src/components/Top.js
@@ -0,0 +1,11 @@
+function Top(prop){
+    return (
+        <div className="top" style={{
+            backgroundColor: prop.color
+        }}>
+            <h1>TOP</h1>
+        </div>
+    );
+}
+
+export default Top;
