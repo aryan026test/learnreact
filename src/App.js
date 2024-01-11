b85dce5a85bb06094acb4639477d65270f0fab4f
chirag dixit <chiragdixit002@gmail.com>
2024-01-11 20:08:10 +0530

    [11/01/24]: Added Box Component

--- src/App.js
+++ src/App.js
@@ -1,48 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
-import Top from './components/Top.js';
-import Bottom from './components/Bottom.js';
-import Left from './components/Left.js';
-import Right from './components/Right.js';
 import Form from './components/Form.js';
-import { useState } from 'react';
+import Box from './components/Box.js';
 
 function App() {
-  // const [x, setX] = useState(0);
-  // const handleValueChange = ()=>{
-  //   setX((prev)=>{
-  //     return prev+1;
-  //   });
-  // }
-
-  // const [y, setY] = useState(10);
-  // const decreaseValue = ()=>{
-  //   setY((prev)=>{
-  //     if(prev>0){
-  //       return prev-1;
-  //     }
-  //     return prev;
-  //   })
-  // }
-
-  // let y = `My name is ${x}`;
-  const [color, setColor] = useState(<Top />);
-  const handleColorChange = ()=>{
-    setColor("blue");
+  const topInfo = {
+    name: "Top",
+    color: "hotpink",
+    height: "100px",
+    width: "98.5%",
+    margin: "auto"
   }
-  
+  const leftInfo = {
+    name: "Left",
+    color: "teal",
+    height: "570px",
+    width: "13%",
+  }
+
+  const rightInfo = {
+    name: "Right",
+    color: "red",
+    height: "570px",
+    width: "13%",
+  }
+  const bottomInfo = {
+    name: "Bottom",
+    color: "orange",
+    height: "570px",
+    width: "70%",
+  }
   return (
     <div className='App'>
-      {/* <h1>{y}</h1>
-      <button onClick={decreaseValue}>click to decrease</button>
-      <h1>{x}</h1>
-      <button onClick={handleValueChange}>Click Me!</button> */}
-      
-      <Top color="hotpink"/>
+      {/* <Top color="hotpink"/>
       <div className='container'>
         <Left color="teal"/>
         <Bottom color="orange"/>
         <Right color="red"/>
+      </div> */}
+      <Box info={topInfo} />
+      <div className='container'>
+        <Box info={leftInfo} />
+        <Box info={bottomInfo} />
+        <Box info={rightInfo} />
       </div>
       <Form />
     </div>
