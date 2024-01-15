5b40f5c937c73a313db9933eb093a5a448389626
chirag dixit <chiragdixit002@gmail.com>
2024-01-15 19:01:14 +0530

    [15/01/24]: Added Routing

---
+++ src/pages/FeaturedProducts.js
@@ -0,0 +1,7 @@
+export const Featured = () =>{
+    return(
+        <div>
+            This is the Featured Page
+        </div>
+    )
+}
--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -14,6 +14,8 @@ function Navbar(){
                 <NavLink style={navLinkStyles} to='/'><h6>Home Icon</h6></NavLink>
                 <NavLink style={navLinkStyles} to='/'><h5>Home</h5></NavLink>
                 <NavLink style={navLinkStyles} to='/about'><h5>About</h5></NavLink>         
+                <NavLink style={navLinkStyles} to='/profile'><h5>Profile</h5></NavLink>         
+                <NavLink style={navLinkStyles} to='/login'><h5>Login</h5></NavLink>         
             </div>
             <NavLink style={navLinkStyles} to='/cart'><h6>Cart Icon</h6></NavLink>
         </div>
